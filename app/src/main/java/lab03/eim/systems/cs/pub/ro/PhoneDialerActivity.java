d5ce529289b55e6a9fd150ba3aab199611d81ae4
ovidiu-dancila <ovidiu.dancila@gmail.com>
2019-03-08 18:27:01 +0200

    implemented taks for laboratory 03

--- app/src/main/java/lab03/eim/systems/cs/pub/ro/PhoneDialerActivity.java
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/PhoneDialerActivity.java
@@ -2,9 +2,9 @@ package lab03.eim.systems.cs.pub.ro;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.provider.SyncStateContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,44 +21,42 @@ public class PhoneDialerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_dialer);
 
+        // Uncommend this to lock the orientation to Portrait
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         final EditText editText = findViewById(R.id.edit_text);
 
-        final Button button1 = findViewById(R.id.button_1);
-        final Button button2 = findViewById(R.id.button_2);
-        final Button button3 = findViewById(R.id.button_3);
-        final Button button4 = findViewById(R.id.button_4);
-        final Button button5 = findViewById(R.id.button_5);
-        final Button button6 = findViewById(R.id.button_6);
-        final Button button7 = findViewById(R.id.button_7);
-        final Button button8 = findViewById(R.id.button_8);
-        final Button button9 = findViewById(R.id.button_9);
-        final Button button0 = findViewById(R.id.button_0);
+        int buttons[] = {R.id.button_0,
+                R.id.button_1,
+                R.id.button_2,
+                R.id.button_3,
+                R.id.button_4,
+                R.id.button_5,
+                R.id.button_6,
+                R.id.button_7,
+                R.id.button_8,
+                R.id.button_9,
+                R.id.button_star,
+                R.id.button_hash};
 
-        button1.setOnClickListener(new ListenerDialer(editText, button1));
-        button2.setOnClickListener(new ListenerDialer(editText, button2));
-        button3.setOnClickListener(new ListenerDialer(editText, button3));
-        button4.setOnClickListener(new ListenerDialer(editText, button4));
-        button5.setOnClickListener(new ListenerDialer(editText, button5));
-        button6.setOnClickListener(new ListenerDialer(editText, button6));
-        button7.setOnClickListener(new ListenerDialer(editText, button7));
-        button8.setOnClickListener(new ListenerDialer(editText, button8));
-        button9.setOnClickListener(new ListenerDialer(editText, button9));
-        button0.setOnClickListener(new ListenerDialer(editText, button0));
+
+        for (int buttonIndex : buttons) {
+            final Button button = findViewById(buttonIndex);
+            button.setOnClickListener(new ListenerDialer(editText, button));
+        }
 
 
         ImageButton button_back = findViewById(R.id.back);
-
         button_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText.getText().length() > 0)
-                    editText.setText(editText.getText().toString().substring(0, editText.getText().length() - 1));
+                int textLen = editText.getText().length();
+                if (textLen > 0)
+                    editText.getText().delete(textLen - 1, textLen);
             }
         });
 
-
         ImageButton button_call = findViewById(R.id.call);
-
         button_call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,7 +74,13 @@ public class PhoneDialerActivity extends AppCompatActivity {
         });
 
         ImageButton button_end_call = findViewById(R.id.end_call);
-        
+        button_end_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 
